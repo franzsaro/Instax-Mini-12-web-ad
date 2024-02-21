5881928d232b901a56d4732edd981589c345bd06
franzsaro <cacayanfranzsaro@gmail.com>
2024-02-21 22:57:28 +0800

    CameraSlider
    Added the camera slider animations

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
 function toggleMenu(){
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
